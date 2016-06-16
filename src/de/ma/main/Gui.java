cea1f55047c751034fa7df17622dac04c59c159e
B4RF <B4RF@LAPTOP-3JLP4UIO.fritz.box>
2016-06-16 17:11:38 +0200

    satisfying modals are now in a seperate class which allows multiple instances

--- src/de/ma/main/Gui.java
+++ src/de/ma/main/Gui.java
@@ -1,6 +1,5 @@
 package de.ma.main;
 
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,19 +7,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import javax.swing.AbstractListModel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 
 import de.ma.ast.Node;
 import de.ma.lexer.Lexer;
@@ -28,14 +22,13 @@ import de.ma.modal.GenerateGraphs;
 import de.ma.modal.Graph;
 import de.ma.modal.LabelGraph;
 import de.ma.modal.Modal;
+import de.ma.modal.SatisfyingModals;
 import de.ma.modal.Vertex;
 import de.ma.parser.Parser;
-import de.ma.treewalker.NNFTreeWalker;
-import de.ma.treewalker.ReduceTreeWalker;
-import de.ma.treewalker.StringTreeWalker;
 
 public class Gui extends JFrame {
 	private static final long serialVersionUID = 1L;
+	
 	static JPanel mainPanel = new JPanel();
 	static JPanel formulaPanel = new JPanel();
 	static JPanel conditionPanel1 = new JPanel();
@@ -46,14 +39,9 @@ public class Gui extends JFrame {
 	static JCheckBox transitive = new JCheckBox("transitive");
 	static JCheckBox serial = new JCheckBox("serial");
 	static JButton enumerate = new JButton("Enumerate");
-	static JLabel satisfiable = new JLabel("-");
-
+	
 	static Lexer lex = new Lexer();
 	static Parser parser = new Parser(lex);
-
-	// Elemente der Auflistung
-	static ArrayList<Modal> enumModals = new ArrayList<Modal>();
-	int modalIndex;
 
 	public Gui() {
 		setTitle("Modallogic Enumerator");
@@ -66,7 +54,6 @@ public class Gui extends JFrame {
 		mainPanel.add(conditionPanel1);
 		mainPanel.add(conditionPanel2);
 		mainPanel.add(enumerate);
-		// mainPanel.add(satPanel);
 
 		formulaPanel.setLayout(new GridLayout(1, 2));
 		formulaPanel.add(new JLabel(" Insert modallogic formula:  "));
@@ -80,12 +67,6 @@ public class Gui extends JFrame {
 		conditionPanel2.add(transitive);
 		conditionPanel2.add(serial);
 
-		// TODO remove?
-		// satPanel.setLayout(new GridLayout(1, 2));
-		// satPanel.add(new JLabel(" Is the formula satisfiable? "));
-		// satPanel.add(satisfiable);
-		// satisfiable.setHorizontalAlignment(SwingConstants.CENTER);
-
 		this.pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
@@ -94,37 +75,28 @@ public class Gui extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				satisfiable.setText("-");
-				enumModals.clear();
-
 				final String input = formula.getText();
 
 				if (input.equals(""))
 					JOptionPane.showMessageDialog(new JFrame(), "Please insert a formula first.");
 				else {
 					
-					Thread t = new Thread(new Runnable() {
+					new Thread(new Runnable() {
 						public void run() {
 							Node root = parser.formula(input);
 
-							//TODO a|~a need to fix
-							updateSatisfiable(root);
-
-							generateModals(root);
+							ArrayList<Modal> enumModals = generateModals(root);
 
 							if (minimal.isSelected())
-								removeNonMinimal(root);
+								removeNonMinimal(enumModals, root);
 
 							if (enumModals.isEmpty())
 								JOptionPane.showMessageDialog(new JFrame(), "No satisfying modal generated.");
 							else{
-								String formula = computeNNF(root);
-								showModallist(formula);
+								new SatisfyingModals(enumModals, root);
 							}
 						}
-					});
-					
-					t.start();
+					}).start();
 				}
 			}
 		});
@@ -135,12 +107,8 @@ public class Gui extends JFrame {
 
 	}
 
-	protected void generateModals(Node root) {
-		// System.out.println("\nFormel erfolgreich geparst");
-		//
-		// StringDepthTreeWalker sdtw = new StringDepthTreeWalker();
-		// System.out.println(sdtw.walk(root, ""));
-
+	protected ArrayList<Modal> generateModals(Node root) {
+		ArrayList<Modal> enumModals = new ArrayList<Modal>();
 		int maxDegree = root.getMaxDegree();
 		int diameter = root.getModalDepth() * 2;
 
@@ -159,43 +127,16 @@ public class Gui extends JFrame {
 
 				// TODO remove/nur fuer tests
 				if (!m.mlMc(root))
-					System.out.println("Failed to ModalCheck a generated Modal");
+					System.out.println("Error: Failed to ModalCheck a generated Modal.");
 			}
 
 			currentGraph = genG.nextGraph();
 		}
-	}
-
-	private String computeNNF(Node root) {
-		ReduceTreeWalker rtw = new ReduceTreeWalker();
-		root = rtw.walk(root, null);
-		NNFTreeWalker ntw = new NNFTreeWalker();
-		root = ntw.walk(root, false);
-
-		StringTreeWalker stw = new StringTreeWalker();
-		String formula = stw.walk(root, "");
-
-		formula = formula.replace("&", "\u2227");
-		formula = formula.replace("|", "\u2228");
-		formula = formula.replace("~>", "\u2227\u00AC"); // TODO change?
-		formula = formula.replace("~", "\u00AC");
-		formula = formula.replace("<->", "\u2194");
-		formula = formula.replace("->", "\u2192");
-		formula = formula.replace("+", "\u2295");
-		formula = formula.replace("$", "\u25CA");
-		formula = formula.replace("#", "\u25A1");
 		
-		return formula;
+		return enumModals;
 	}
 
-	protected void updateSatisfiable(Node root) {
-		if (root.mlSat())
-			satisfiable.setText("Yes");
-		else
-			satisfiable.setText("No");
-	}
-
-	private void removeNonMinimal(Node root) {
+	private void removeNonMinimal(ArrayList<Modal> enumModals, Node root) {
 		Iterator<Modal> iter = enumModals.iterator();
 		while (iter.hasNext()) {
 			Modal modal = iter.next();
@@ -214,52 +155,5 @@ public class Gui extends JFrame {
 				}
 			}
 		}
-	}
-
-	private void showModallist(String formula) {
-		final JFrame enumFrame = new JFrame();
-		final JList<String> enumList = new JList<>();
-		final JLabel imageLabel = new JLabel();
-		final JLabel formulaLabel = new JLabel(formula, SwingConstants.CENTER);
-
-		enumList.setModel(new AbstractListModel<String>() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public String getElementAt(int i) {
-				return "Graph " + i;
-			}
-
-			@Override
-			public int getSize() {
-				return enumModals.size();
-			}
-		});
-
-		enumList.addListSelectionListener(new ListSelectionListener() {
-
-			@Override
-			public void valueChanged(ListSelectionEvent evt) {
-				int index = Integer.parseInt(enumList.getSelectedValue().substring(6));
-
-				if (index != modalIndex) {
-					modalIndex = index;
-					Modal m = enumModals.get(modalIndex);
-					m.draw(imageLabel);
-					enumFrame.pack();
-				}
-			}
-		});
-
-		enumFrame.getContentPane().add(formulaLabel, BorderLayout.PAGE_START);
-		enumFrame.getContentPane().add(new JScrollPane(enumList), BorderLayout.LINE_START);
-		enumFrame.getContentPane().add(imageLabel, BorderLayout.CENTER);
-		
-		enumModals.get(0).draw(imageLabel);
-		
-		enumFrame.setTitle("Satisfying Models");
-		enumFrame.pack();
-		enumFrame.setLocationRelativeTo(null);
-		enumFrame.setVisible(true);
 	}
 }
---
+++ src/de/ma/modal/SatisfyingModals.java
@@ -0,0 +1,97 @@
+package de.ma.modal;
+
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+
+import javax.swing.AbstractListModel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import de.ma.ast.Node;
+import de.ma.treewalker.NNFTreeWalker;
+import de.ma.treewalker.ReduceTreeWalker;
+import de.ma.treewalker.StringTreeWalker;
+
+public class SatisfyingModals extends JFrame{
+	private static final long serialVersionUID = 1L;
+	
+	ArrayList<Modal> enumModals;
+	String formula;
+	
+	JList<String> enumList = new JList<>();
+	JLabel imageLabel = new JLabel();
+
+	int modalIndex;
+	
+	public SatisfyingModals(ArrayList<Modal> enumM, Node root) {
+		this.enumModals = enumM;
+		this.formula = computeNNF(root);
+
+		enumList.setModel(new AbstractListModel<String>() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public String getElementAt(int i) {
+				return "Graph " + i;
+			}
+
+			@Override
+			public int getSize() {
+				return enumModals.size();
+			}
+		});
+
+		enumList.addListSelectionListener(new ListSelectionListener() {
+			
+			@Override
+			public void valueChanged(ListSelectionEvent evt) {
+				int index = Integer.parseInt(enumList.getSelectedValue().substring(6));
+
+				if (index != modalIndex) {
+					modalIndex = index;
+					Modal m = enumModals.get(modalIndex);
+					m.draw(imageLabel);
+					pack();
+				}
+			}
+		});
+
+		this.getContentPane().add(new JLabel(formula, SwingConstants.CENTER), BorderLayout.PAGE_START);
+		this.getContentPane().add(new JScrollPane(enumList), BorderLayout.LINE_START);
+		this.getContentPane().add(imageLabel, BorderLayout.CENTER);
+		
+		enumModals.get(0).draw(imageLabel);
+		
+		setTitle("Satisfying Models");
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	private String computeNNF(Node root) {
+		ReduceTreeWalker rtw = new ReduceTreeWalker();
+		root = rtw.walk(root, null);
+		NNFTreeWalker ntw = new NNFTreeWalker();
+		root = ntw.walk(root, false);
+
+		StringTreeWalker stw = new StringTreeWalker();
+		String formula = stw.walk(root, "");
+
+		formula = formula.replace("&", "\u2227");
+		formula = formula.replace("|", "\u2228");
+		formula = formula.replace("~>", "\u2227\u00AC"); // TODO change?
+		formula = formula.replace("~", "\u00AC");
+		formula = formula.replace("<->", "\u2194");
+		formula = formula.replace("->", "\u2192");
+		formula = formula.replace("+", "\u2295");
+		formula = formula.replace("$", "\u25CA");
+		formula = formula.replace("#", "\u25A1");
+		
+		return formula;
+	}
+}
