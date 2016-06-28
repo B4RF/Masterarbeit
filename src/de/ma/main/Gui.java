4a1972f5a498406985fa1873c2460cab39a22268
B4RF <B4RF@LAPTOP-3JLP4UIO.fritz.box>
2016-06-28 16:48:38 +0200

    moved some operations from gui to satisfyingmodals

--- src/de/ma/main/Gui.java
+++ src/de/ma/main/Gui.java
@@ -5,8 +5,6 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -20,12 +18,7 @@ import javax.swing.SwingConstants;
 
 import de.ma.ast.Node;
 import de.ma.lexer.Lexer;
-import de.ma.modal.GenerateGraphs;
-import de.ma.modal.Graph;
-import de.ma.modal.LabelGraph;
-import de.ma.modal.Modal;
 import de.ma.modal.SatisfyingModals;
-import de.ma.modal.Vertex;
 import de.ma.parser.Parser;
 
 public class Gui extends JFrame {
@@ -34,7 +27,6 @@ public class Gui extends JFrame {
 	static JPanel mainPanel = new JPanel(new GridBagLayout());
 	static JTextField formula = new JTextField();
 	static JPanel optionPanel = new JPanel(new GridBagLayout());
-	static JCheckBox minimal = new JCheckBox("minimal", true);
 	static JCheckBox reflexive = new JCheckBox("reflexive");
 	static JCheckBox transitive = new JCheckBox("transitive");
 	static JCheckBox serial = new JCheckBox("serial");
@@ -97,35 +89,29 @@ public class Gui extends JFrame {
 		c.gridx = 0;
 		c.gridy = 0;
 		c.weightx = 0.33;
-		optionPanel.add(minimal, c);
+		optionPanel.add(reflexive, c);
 
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 1;
-		c.gridy = 0;
-		c.weightx = 0.33;
-		optionPanel.add(serial, c);
-
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 2;
 		c.gridy = 0;
 		c.weightx = 0.33;
 		optionPanel.add(transitive, c);
 
 		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 0;
-		c.gridy = 1;
-		c.weightx = 0.5;
-		optionPanel.add(reflexive, c);
+		c.gridx = 2;
+		c.gridy = 0;
+		c.weightx = 0.33;
+		optionPanel.add(serial, c);
 
 		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 1;
+		c.gridx = 0;
 		c.gridy = 1;
 		c.weightx = 0.5;
 		optionPanel.add(orbits, c);
 
 		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 0;
-		c.gridy = 2;
+		c.gridx = 1;
+		c.gridy = 1;
 		c.weightx = 1;
 		c.gridwidth = 2;
 		optionPanel.add(partReflexive, c);
@@ -143,18 +129,13 @@ public class Gui extends JFrame {
 				if (input.equals(""))
 					JOptionPane.showMessageDialog(new JFrame(), "Please insert a formula first.");
 				else {
+					final Node root = parser.formula(input);
 
+					
 					new Thread(new Runnable() {
 						public void run() {
-							Node root = parser.formula(input);
-
-							ArrayList<Modal> enumModals = generateModals(root);
-
-							if (enumModals.isEmpty())
-								JOptionPane.showMessageDialog(new JFrame(), "No satisfying modal generated.");
-							else {
-								new SatisfyingModals(enumModals, root);
-							}
+							new SatisfyingModals(root, reflexive.isSelected(),
+									transitive.isSelected(), serial.isSelected(), partReflexive.isSelected(), orbits.isSelected());
 						}
 					}).start();
 				}
@@ -188,63 +169,5 @@ public class Gui extends JFrame {
 	public static void main(String[] args) throws IOException {
 		new Gui();
 
-	}
-
-	protected ArrayList<Modal> generateModals(Node root) {
-		ArrayList<Modal> enumModals = new ArrayList<Modal>();
-		int maxDegree = root.getMaxDegree();
-		int diameter = root.getModalDepth() * 2;
-		int maxVertices = 0;
-
-		if (minimal.isSelected()) {
-			// maximal vertices for minimal graph is equals number of diamonds
-			// plus one
-			maxVertices = input.length() - input.replace("$", "").length() + 1;
-		}
-
-		GenerateGraphs genG = new GenerateGraphs(maxDegree, diameter, maxVertices, reflexive.isSelected(),
-				transitive.isSelected(), serial.isSelected(), partReflexive.isSelected(), orbits.isSelected());
-		LabelGraph labelG = new LabelGraph(orbits.isSelected());
-		Graph currentGraph;
-
-		while ((currentGraph = genG.nextGraph()) != null) {
-			Modal modal = new Modal(currentGraph);
-
-			ArrayList<Modal> labeled = labelG.labelGraph(modal, root);
-
-			if (minimal.isSelected())
-				removeNonMinimal(labeled, root);
-
-			for (Modal m : labeled) {
-				enumModals.add(m);
-
-				// if (!m.mlMc(root))
-				// System.out.println("Error: Failed to ModalCheck a generated
-				// Modal.");
-			}
-		}
-
-		return enumModals;
-	}
-
-	private void removeNonMinimal(ArrayList<Modal> enumModals, Node root) {
-		Iterator<Modal> iter = enumModals.iterator();
-		while (iter.hasNext()) {
-			Modal modal = iter.next();
-
-			modalloop: for (Integer index : modal.getVertices()) {
-				Vertex v = modal.getGraph().getVertex(index);
-
-				for (Integer edge : v.getEdges()) {
-					Modal m = modal.clone();
-					m.getGraph().removeEdge(index, edge);
-
-					if (m.mlMc(root)) {
-						iter.remove();
-						break modalloop;
-					}
-				}
-			}
-		}
 	}
 }
--- src/de/ma/modal/SatisfyingModals.java
+++ src/de/ma/modal/SatisfyingModals.java
@@ -2,6 +2,7 @@ package de.ma.modal;
 
 import java.awt.BorderLayout;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.AbstractListModel;
 import javax.swing.JFrame;
@@ -20,17 +21,31 @@ import de.ma.treewalker.StringTreeWalker;
 public class SatisfyingModals extends JFrame{
 	private static final long serialVersionUID = 1L;
 	
-	ArrayList<Modal> enumModals;
+	ArrayList<Modal> enumModals = new ArrayList<>();
+	Node root;
 	String formula;
+	boolean reflexive;
+	boolean transitive;
+	boolean serial;
+	boolean partialReflexive;
+	boolean useOrbits;
 	
 	JList<String> enumList = new JList<>();
 	JLabel imageLabel = new JLabel();
 
 	int modalIndex;
 	
-	public SatisfyingModals(ArrayList<Modal> enumM, Node root) {
-		this.enumModals = enumM;
+	public SatisfyingModals(Node rt, boolean ref, boolean trans, boolean ser, boolean partRef, boolean orb) {
+		this.root = rt;
 		this.formula = computeNNF(root);
+		
+		this.reflexive = ref;
+		this.transitive = trans;
+		this.serial = ser;
+		this.partialReflexive = partRef;
+		this.useOrbits = orb;
+		
+		enumModals = generateModals(root);
 
 		enumList.setModel(new AbstractListModel<String>() {
 			private static final long serialVersionUID = 1L;
@@ -61,8 +76,8 @@ public class SatisfyingModals extends JFrame{
 			}
 		});
 
-		this.getContentPane().add(new JLabel(formula, SwingConstants.CENTER), BorderLayout.PAGE_START);
-		this.getContentPane().add(new JScrollPane(enumList), BorderLayout.LINE_START);
+		this.getContentPane().add(new JLabel(formula, SwingConstants.CENTER), BorderLayout.NORTH);
+		this.getContentPane().add(new JScrollPane(enumList), BorderLayout.WEST);
 		this.getContentPane().add(imageLabel, BorderLayout.CENTER);
 		
 		enumModals.get(0).draw(imageLabel);
@@ -93,5 +108,64 @@ public class SatisfyingModals extends JFrame{
 		formula = formula.replace("#", "\u25A1");
 		
 		return formula;
+	}
+
+	protected ArrayList<Modal> generateModals(Node root) {
+		ArrayList<Modal> enumModals = new ArrayList<Modal>();
+		int maxDegree = root.getMaxDegree();
+		int diameter = root.getModalDepth() * 2;
+		int maxVertices = 0;
+
+		// TODO
+		// maximal vertices for minimal graph is equals number of diamonds
+		// plus one
+		StringTreeWalker stw = new StringTreeWalker();
+		String input = stw.walk(root, null);
+		maxVertices = input.length() - input.replace("$", "").length() + 1;
+
+		GenerateGraphs genG = new GenerateGraphs(maxDegree, diameter, maxVertices, reflexive,
+				transitive, serial, partialReflexive, useOrbits);
+		LabelGraph labelG = new LabelGraph(useOrbits);
+		Graph currentGraph;
+
+		while ((currentGraph = genG.nextGraph()) != null) {
+			Modal modal = new Modal(currentGraph);
+
+			ArrayList<Modal> labeled = labelG.labelGraph(modal, root);
+
+			// TODO
+			removeNonMinimal(labeled, root);
+
+			for (Modal m : labeled) {
+				enumModals.add(m);
+
+				// if (!m.mlMc(root))
+				// System.out.println("Error: Failed to ModalCheck a generated
+				// Modal.");
+			}
+		}
+
+		return enumModals;
+	}
+
+	private void removeNonMinimal(ArrayList<Modal> enumModals, Node root) {
+		Iterator<Modal> iter = enumModals.iterator();
+		while (iter.hasNext()) {
+			Modal modal = iter.next();
+
+			modalloop: for (Integer index : modal.getVertices()) {
+				Vertex v = modal.getGraph().getVertex(index);
+
+				for (Integer edge : v.getEdges()) {
+					Modal m = modal.clone();
+					m.getGraph().removeEdge(index, edge);
+
+					if (m.mlMc(root)) {
+						iter.remove();
+						break modalloop;
+					}
+				}
+			}
+		}
 	}
 }
