e08fae5d6afa5b023cbfba583415765605d20ce1
B4RF <B4RF@LAPTOP-3JLP4UIO.fritz.box>
2016-07-03 15:33:51 +0200

    replaced the generating thread with a swingworked (no more blank lists)

--- src/de/ma/modal/LabelGraph.java
+++ src/de/ma/modal/LabelGraph.java
@@ -17,78 +17,79 @@ import de.ma.treewalker.NNFTreeWalker;
 public class LabelGraph {
 	private boolean useOrbits;
 
-	public LabelGraph(boolean orb){
+	public LabelGraph(boolean orb) {
 		this.useOrbits = orb;
 	}
-	
-	public ArrayList<Modal> labelGraph(Modal modal, Node root){
+
+	public ArrayList<Modal> labelGraph(Modal modal, Node root) {
 		AndOrTreeWalker aotw = new AndOrTreeWalker();
 		root = aotw.walk(root, null);
 		NNFTreeWalker nnf = new NNFTreeWalker();
 		root = nnf.walk(root, false);
-		
+
 		return labelNNFGraph(modal, modal.getGraph().getInitVertex(), root);
 	}
-	
-	private ArrayList<Modal> labelNNFGraph(Modal modal, Vertex vertex, Node root){
+
+	private ArrayList<Modal> labelNNFGraph(Modal modal, Vertex vertex, Node root) {
 		ArrayList<Modal> labeled = new ArrayList<>();
 		modal = modal.clone();
 		String prefix = "";
-		
-		switch(root.getToken().tag){
+
+		switch (root.getToken().tag) {
 		case '#':
 			Box box = (Box) root;
-			
+
 			// box without successor is fulfilled
-			if(vertex.getEdges().size() == 0)
+			if (vertex.getEdges().size() == 0)
 				labeled.add(modal);
 
 			ArrayList<Modal> combinedModals = new ArrayList<>();
 			for (Integer index : vertex.getEdges()) {
 				Vertex edge = modal.getGraph().getVertex(index);
 				ArrayList<Modal> currentModals = labelNNFGraph(modal, edge, box.getNode());
-				
+
 				// if one edge doesn't satisfy, the box operator doesn't as well
-				if(currentModals.isEmpty()){
+				if (currentModals.isEmpty()) {
 					combinedModals.clear();
 					break;
 				}
-				
-				//TODO check for isomorphic subgraphs
-				
-				if(combinedModals.isEmpty())
+
+				// TODO check for isomorphic subgraphs
+
+				if (combinedModals.isEmpty())
 					combinedModals = currentModals;
 				else
 					combinedModals = combine(combinedModals, currentModals);
 			}
-			
+
 			labeled.addAll(combinedModals);
 			break;
 		case '$':
 			Diamond diam = (Diamond) root;
-			
+
 			for (Integer index : vertex.getEdges()) {
 				Vertex edge = modal.getGraph().getVertex(index);
 
 				labeled.addAll(labelNNFGraph(modal, edge, diam.getNode()));
 			}
+
 			break;
 		case Tag.BICONDITION:
 			// (a&b)|(~a&~b)
-//			break;
+			// break;
 		case Tag.IMPLICATION:
 			// ~a|b
-//			break;
+			// break;
 		case Tag.NEGIMPLICATION:
 			// a&~b
-//			break;
+			// break;
 		case '+':
 			// (~a&b)|(a&~b)
 			System.out.println("Fail: Wrong operator while labelling.");
 			break;
 		case '|':
 			Or or = (Or) root;
-			
+
 			ArrayList<Modal> leftOr = labelNNFGraph(modal, vertex, or.getLeft());
 			ArrayList<Modal> rightOr = labelNNFGraph(modal, vertex, or.getRight());
 
@@ -100,7 +101,7 @@ public class LabelGraph {
 			}
 			for (Modal mLeft : leftOr) {
 				for (Modal mRight : rightOr) {
-					if(mLeft.isCompatible(mRight)){
+					if (mLeft.isCompatible(mRight)) {
 						Modal joined = mLeft.clone();
 						joined.join(mRight);
 						labeled.add(joined);
@@ -112,10 +113,10 @@ public class LabelGraph {
 			And and = (And) root;
 			ArrayList<Modal> leftAnd = labelNNFGraph(modal, vertex, and.getLeft());
 			ArrayList<Modal> rightAnd = labelNNFGraph(modal, vertex, and.getRight());
-			
+
 			for (Modal mLeft : leftAnd) {
 				for (Modal mRight : rightAnd) {
-					if(mLeft.isCompatible(mRight)){
+					if (mLeft.isCompatible(mRight)) {
 						Modal joined = mLeft.clone();
 						joined.join(mRight);
 						labeled.add(joined);
@@ -133,31 +134,29 @@ public class LabelGraph {
 			Variable variable = (Variable) root;
 			String var = prefix + ((Word) variable.getToken()).getVarible();
 
-			if(modal.addVarToVertex(var, vertex.getIndex()))
+			if (modal.addVarToVertex(var, vertex.getIndex()))
 				labeled.add(modal);
 			break;
 		default:
 			throw new Error("Token error while labeling");
 		}
-		
-		
+
 		return labeled;
 	}
 
-	private ArrayList<Modal> combine(ArrayList<Modal> modalList1,
-			ArrayList<Modal> modalList2) {
+	private ArrayList<Modal> combine(ArrayList<Modal> modalList1, ArrayList<Modal> modalList2) {
 		ArrayList<Modal> combinedModals = new ArrayList<>();
-		
+
 		for (Modal m1 : modalList1) {
 			for (Modal m2 : modalList2) {
-				if(m1.isCompatible(m2)){
+				if (m1.isCompatible(m2)) {
 					Modal joined = m1.clone();
 					joined.join(m2);
 					combinedModals.add(joined);
 				}
 			}
 		}
-		
+
 		return combinedModals;
 	}
 }
--- src/de/ma/modal/SatisfyingModals.java
+++ src/de/ma/modal/SatisfyingModals.java
@@ -2,14 +2,17 @@ package de.ma.modal;
 
 import java.awt.BorderLayout;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
+import javax.swing.SwingWorker;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -66,12 +69,89 @@ public class SatisfyingModals extends JFrame {
 		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
+		
+		Generator g = new Generator(this);
+		g.execute();
+	}
+	
+	private class Generator extends SwingWorker<List<String>, Modal>{
+		
+		JFrame frame;
+		
+		public  Generator(JFrame f){
+			this.frame = f;
+		}
 
-		new Thread(new Runnable() {
-			public void run() {
-				generateModals();
+		@Override
+		protected List<String> doInBackground() throws Exception {
+			JProgressBar progress = new JProgressBar();
+			getContentPane().add(progress, BorderLayout.SOUTH);
+
+			// TODO change
+			int maxDegree = root.getMaxDegree();
+			int diameter = root.getModalDepth() * 2;
+
+			StringTreeWalker stw = new StringTreeWalker();
+			String input = stw.walk(root, null);
+			// maximal vertices for minimal graph is equals number of diamonds
+			// plus one
+			int maxVertices = input.length() - input.replace("$", "").length() + 1;
+
+			GenerateGraphs genG = new GenerateGraphs(progress, maxDegree, diameter, maxVertices, reflexive, transitive, serial,
+					partialReflexive, useOrbits);
+
+			LabelGraph labelG = new LabelGraph(useOrbits);
+			Graph currentGraph;
+
+			while ((currentGraph = genG.nextGraph()) != null) {
+				ArrayList<Modal> labeled = labelG.labelGraph(new Modal(currentGraph), root);
+
+				boolean minimal;
+
+				for (Modal m : labeled) {
+					minimal = true;
+
+					modalloop: for (Integer index : m.getVertices()) {
+						Vertex v = m.getGraph().getVertex(index);
+
+						for (Integer edge : v.getEdges()) {
+							Modal mClone = m.clone();
+							mClone.getGraph().removeEdge(index, edge);
+
+							if (mClone.mlMc(root)) {
+								minimal = false;
+								break modalloop;
+							}
+						}
+					}
+
+					if (minimal) {
+						publish(m);
+
+						if (enumModals.size() == 1) {
+							enumModals.get(0).draw(imageLabel);
+							pack();
+						}
+					}
+				}
 			}
-		}).start();
+			
+			if(enumModals.size() == 0){
+				frame.dispose();
+				JOptionPane.showMessageDialog(frame, "No satisfying modal generated.");
+			}else{
+				frame.remove(progress);
+			}
+			return null;
+		}
+
+		@Override
+		protected void process(List<Modal> chunks) {
+			for (Modal m : chunks) {
+				enumModals.add(m);
+				listModel.addElement("Graph " + (enumModals.size() - 1));
+			}
+		}
 	}
 
 	private String computeNNF() {
@@ -94,62 +174,5 @@ public class SatisfyingModals extends JFrame {
 		nnf = nnf.replace("#", "\u25A1");
 
 		return nnf;
-	}
-
-	private void generateModals() {
-		JProgressBar progress = new JProgressBar();
-		getContentPane().add(progress, BorderLayout.SOUTH);
-
-		// TODO change
-		int maxDegree = root.getMaxDegree();
-		int diameter = root.getModalDepth() * 2;
-
-		StringTreeWalker stw = new StringTreeWalker();
-		String input = stw.walk(root, null);
-		// maximal vertices for minimal graph is equals number of diamonds
-		// plus one
-		int maxVertices = input.length() - input.replace("$", "").length() + 1;
-
-		GenerateGraphs genG = new GenerateGraphs(progress, maxDegree, diameter, maxVertices, reflexive, transitive, serial,
-				partialReflexive, useOrbits);
-
-		LabelGraph labelG = new LabelGraph(useOrbits);
-		Graph currentGraph;
-
-		while ((currentGraph = genG.nextGraph()) != null) {
-			ArrayList<Modal> labeled = labelG.labelGraph(new Modal(currentGraph), root);
-
-			boolean minimal;
-
-			for (Modal m : labeled) {
-				minimal = true;
-
-				modalloop: for (Integer index : m.getVertices()) {
-					Vertex v = m.getGraph().getVertex(index);
-
-					for (Integer edge : v.getEdges()) {
-						Modal mClone = m.clone();
-						mClone.getGraph().removeEdge(index, edge);
-
-						if (mClone.mlMc(root)) {
-							minimal = false;
-							break modalloop;
-						}
-					}
-				}
-
-				if (minimal) {
-					enumModals.add(m);
-					listModel.addElement("Graph " + (enumModals.size() - 1));
-
-					if (enumModals.size() == 1) {
-						enumModals.get(0).draw(imageLabel);
-						pack();
-					}
-				}
-			}
-		}
-		
-		this.remove(progress);
 	}
 }
